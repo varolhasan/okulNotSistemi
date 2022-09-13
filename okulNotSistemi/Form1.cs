2791f67d2b0980753d7e45662928c20dc389431e
Hasan VAROL <hasanvarooll@gmail.com>
2022-09-14 02:11:11 +0300

    Add project files.

---
+++ okulNotSistemi/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace okulNotSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            FrmOgrenciNotlar fr1 = new FrmOgrenciNotlar();
+            fr1.ogrenciID = Convert.ToInt32(txtNumara.Text);
+            fr1.Show();
+            this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ okulNotSistemi/FrmOgrenciNotlar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace okulNotSistemi
+{
+    public partial class FrmOgrenciNotlar : Form
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-RP5F7ET;Initial Catalog=OkulSistemi;Integrated Security=True");
+        public FrmOgrenciNotlar()
+        {
+            InitializeComponent();
+        }
+        public int ogrenciID;
+        private void FrmOgrenciNotlar_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("select DERSAD, OGRENCIID, SINAV1, SINAV2, SINAV3, PROJE, ORTALAMA, DURUM from Tbl_Notlar " +
+                "INNER JOIN Tbl_Dersler on Tbl_Notlar.DERSID=Tbl_Dersler.DERSID where OGRENCIID=" + ogrenciID, baglanti);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
